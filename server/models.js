4edc700e9c0a1e1a666c0b4a4509fdd347ad24b3
rebeccaquey <rebecca_quey@yahoo.com>
2020-07-01 14:27:05 -0700

    get random number to display rooms

--- server/models.js
+++ server/models.js
@@ -1,7 +1,14 @@
 const Carousels = require('../database/Carousel.js');
 
+// const getCarousels = (callback) => {
+//   Carousels.find(callback).sort({ _id: -1 }).limit(20);
+// };
+
 const getCarousels = (callback) => {
-  Carousels.find(callback).sort({ _id: -1 }).limit(20);
+  const max = 79;
+  const min = 0;
+  const skipsize = Math.floor(Math.random() * (max - min + 1)) + min;
+  Carousels.find(callback).sort({ _id: -1 }).skip(skipsize).limit(20);
 };
 
 const getSpecificCarousel = (roomId, callback) => {
